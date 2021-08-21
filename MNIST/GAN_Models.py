e901adfee158300842db0705b110206d6c50f43b
Mahantesh Pattadkal <55622066+Mahantesh-Pattadkal-1993@users.noreply.github.com>
2021-08-21 19:53:49 +0200

    # Codes for GAN and Classifier

---
+++ MNIST/GAN_Models.py
@@ -0,0 +1,211 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 12 12:32:32 2021
+
+@author: mpatt
+"""
+
+#Import the libraries 
+import torch
+import torch.optim as opt
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+import torchvision.transforms as transforms
+
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+#%matplotlib inline
+from utils import imshow
+
+
+import argparse
+import os
+import random
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.utils.data
+import torchvision.datasets as dset
+import torchvision.utils as vutils
+
+
+
+
+#MNIST Classifier by Eoin
+
+class CNN(nn.Module):
+	
+	def __init__(self):
+		super(CNN, self).__init__()
+		self.main = nn.Sequential(
+			
+			# input is Z, going into a convolution
+			nn.Conv2d(1, 8, kernel_size=5, stride=1, padding=2),
+			nn.BatchNorm2d(8),
+			nn.ReLU(True),
+			nn.Dropout2d(p=0.1),
+			
+			nn.Conv2d(8, 16, kernel_size=5, stride=2, padding=2),
+			nn.BatchNorm2d(16),
+			nn.ReLU(True),
+			nn.Dropout2d(p=0.1),
+
+			nn.Conv2d(16, 32, kernel_size=5, stride=1, padding=2),
+			nn.BatchNorm2d(32),
+			nn.ReLU(True),
+			nn.Dropout2d(p=0.1),
+
+			nn.Conv2d(32, 64, kernel_size=5, stride=2, padding=2),
+			nn.BatchNorm2d(64),
+			nn.ReLU(True),
+			nn.Dropout2d(p=0.2),
+
+			nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+			nn.BatchNorm2d(128),
+			nn.ReLU(True)
+		)
+			
+		self.classifier = nn.Sequential(
+			nn.Linear(128, 10),
+		)
+		
+	def forward(self, x):   
+		x = self.main(x)
+		x = torch.mean(x.view(x.size(0), x.size(1), -1), dim=2)  # GAP Layer
+		logits = self.classifier(x)
+		return logits, x
+
+
+
+# Classifier by me
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, 3, 1)
+        self.conv2 = nn.Conv2d(32, 64, 3, 1)
+        self.dropout1 = nn.Dropout(0.25)
+        self.dropout2 = nn.Dropout(0.5)
+        self.fc1 = nn.Linear(9216, 128)
+        self.fc2 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = self.dropout1(x)
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.dropout2(x)
+        x = self.fc2(x)
+        output = F.softmax(x, dim=1)
+        return output
+
+
+# GAN without the convolutional operation
+
+class Gen(nn.Module):
+    def __init__(self):
+        super().__init__() # get the init variables from parent class
+        self.model = nn.Sequential(
+            nn.Linear(Z_dim, H_dim),
+            nn.ReLU(),
+            nn.Linear(H_dim, X_dim),
+            nn.Sigmoid()
+        )
+          
+    def forward(self, input):
+        return self.model(input)
+
+
+class Dis(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.model = nn.Sequential(
+            nn.Linear(X_dim, H_dim),
+            nn.ReLU(),
+            nn.Linear(H_dim, 1),
+            nn.Sigmoid()
+        )
+        
+    def forward(self, input):
+        return self.model(input)
+    
+
+
+
+#Using DCGAN architecture
+
+
+class DC_Generator(nn.Module):
+    def __init__(self, nc, nz, ngf):    
+      """
+      nc--- number of channels in output img = 1 for MNIST
+      nz-----dimension of z latent space
+      ngf----size of input image = 28 for MNIST
+      
+      """
+      super(DC_Generator, self).__init__()
+      self.network = nn.Sequential(
+          nn.ConvTranspose2d(nz, ngf*4, 4, 1, 0, bias=False),
+          nn.BatchNorm2d(ngf*4),
+          nn.ReLU(True),
+  
+          nn.ConvTranspose2d(ngf*4, ngf*2, 3, 2, 1, bias=False),
+          nn.BatchNorm2d(ngf*2),
+          nn.ReLU(True),
+  
+          nn.ConvTranspose2d(ngf*2, ngf, 4, 2, 1, bias=False),
+          nn.BatchNorm2d(ngf),
+          nn.ReLU(True),
+  
+          nn.ConvTranspose2d(ngf, nc, 4, 2, 1, bias=False),
+          nn.Tanh()
+      )
+  
+    def forward(self, input):
+      output = self.network(input)
+      return output
+
+class DC_Discriminator(nn.Module):
+    def __init__(self, nc, ndf):
+        """
+        
+
+        Parameters
+        ----------
+        nc : number of channels in output img = 1 for MNIST
+            DESC : if it is a colored image make it 3 as RGB
+        ndf : number of features in input image = 28
+            
+
+        Returns
+        -------
+        None.
+
+        """
+        super(DC_Discriminator, self).__init__()
+        self.network = nn.Sequential(
+                
+                nn.Conv2d(nc, ndf, 4, 2, 1, bias=False),
+                nn.LeakyReLU(0.2, inplace=True),
+                
+                nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
+                nn.BatchNorm2d(ndf * 2),
+                nn.LeakyReLU(0.2, inplace=True),
+                
+                nn.Conv2d(ndf * 2, ndf * 4, 3, 2, 1, bias=False),
+                nn.BatchNorm2d(ndf * 4),
+                nn.LeakyReLU(0.2, inplace=True),
+                
+                nn.Conv2d(ndf * 4, 1, 4, 1, 0, bias=False),
+                nn.Sigmoid()
+            )
+    def forward(self, input):
+        output = self.network(input)
+        return output.view(-1, 1).squeeze(1)
